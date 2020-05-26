435ca1e6d0b88ed21991b622fc942be1bce0cbd7
JimHinson <jim.hinson@live.com>
2020-05-26 18:25:53 -0400

    Remove superflous file

--- src/com/AutomationUniversity/ToDoInterface.java
+++
@@ -1,4 +0,0 @@
-package com.AutomationUniversity;
-
-//public class ToDoInterface {
-//}
---
+++ src/com/AutomationUniversity/TodoAppTest.java
@@ -0,0 +1,23 @@
+package com.AutomationUniversity;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TodoAppTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void main() {
+        
+    }
+
+    @org.junit.jupiter.api.Test
+    void help() {
+    }
+}
